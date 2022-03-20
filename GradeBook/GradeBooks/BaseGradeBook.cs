1d14a878f62e679c9b282c508b0a56f11edb280c
Almaroo <mfxbarszcz@gmail.com>
2022-03-20 19:10:35 +0100

    Add Type property to BaseGradeBook class

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -13,6 +13,7 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public GradeBookType Type { get; protected set; }
 
         public BaseGradeBook(string name)
         {
